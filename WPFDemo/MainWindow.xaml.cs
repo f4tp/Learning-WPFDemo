9bfce4871f5823ca6d003a2656013483ad81e261
f4tp <mr_treadwell@live.co.uk>
2018-03-28 19:09:51 +0100

    Video finished, an (ineffective) class added to test functionality

--- WPFDemo/MainWindow.xaml.cs
+++ WPFDemo/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 namespace WPFDemo
 {
     /// <summary>
@@ -23,6 +25,20 @@ namespace WPFDemo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("hello");
+            Person person1 = new Person("Steve", 40, 23.5m);
+            Person person2 = new Person("Andy", 23, 42.565m);
+            Person.CycleThroughPersonsInList();
+
+        }
+
+        private void submitButton_DragOver(object sender, DragEventArgs e)
+        {
+            MessageBox.Show("I have been dragged over");
         }
     }
 }
---
+++ WPFDemo/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFDemo
+{
+    class Person
+    {
+        private static List<Person> allPeopleList;
+        public string name { get; set; }
+        public int age { get; set; }
+        public decimal heightInMeters { get; set; }
+        public Person(string Name, int Age, decimal HeightInMeters)
+        {
+            this.name = Name;
+            this.age = age;
+            this.heightInMeters = HeightInMeters;
+            AddPersonToTheList(this);
+        }
+
+        private static void AddPersonToTheList(Person personin)
+        {
+            if (allPeopleList == null)
+            {
+                allPeopleList = new List<Person>();
+                allPeopleList.Add(personin);
+            }
+            else
+            {
+                allPeopleList.Add(personin);
+            }
+
+
+        }
+        public static List<Person> GetAllPeople()
+        {
+            if (allPeopleList != null)
+            {
+                return allPeopleList;
+            }
+            else
+            {
+                allPeopleList = new List<Person>();
+                return allPeopleList;
+            }
+        }
+
+        public static void CycleThroughPersonsInList()
+        {
+            foreach (Person p in allPeopleList)
+            {
+                String forMSShow  = p.name + ", " + p.age.ToString() + ", " + p.heightInMeters.ToString();
+                MessageBox.Show(forMSShow);
+            }
+        }
+       
+
+    }
+}
+        
+
